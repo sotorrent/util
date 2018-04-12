5a9e4468a3b8bd3b77227036db5a75e8d78c9883
Sebastian Baltes <research@sbaltes.com>
2018-04-12 14:55:43 +0930

    Add split method and deploy new version v1.0.7

--- src/de/unitrier/st/util/Util.java
+++ src/de/unitrier/st/util/Util.java
@@ -166,4 +166,15 @@ public class Util {
         return stringWriter.toString();
     }
 
+    public static List<Integer>[] split(List<Integer> integers, int partitionCount) {
+        int partitionSize = (int)Math.ceil((double)integers.size()/partitionCount);
+        @SuppressWarnings("unchecked")
+        List<Integer>[] subLists = new List[partitionCount];
+        int subListIndex = 0;
+        for (int i=0; i<integers.size(); i+=partitionSize) {
+            subLists[subListIndex] = integers.subList(i, Math.min(integers.size(), i+partitionSize));
+            subListIndex++;
+        }
+        return subLists;
+    }
 }
