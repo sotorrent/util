44a7a0c1d91bf70129e2a062bbbda1e5af1f266e
Sebastian Baltes <research@sbaltes.com>
2018-05-15 17:10:21 +0200

    Move URL regex to separate variable
    * needed in project so-posthistory-extractor

--- src/de/unitrier/st/util/Patterns.java
+++ src/de/unitrier/st/util/Patterns.java
@@ -5,7 +5,9 @@ import java.util.regex.Pattern;
 public class Patterns {
     // for the basic regex, see https://stackoverflow.com/a/6041965, alternative: https://stackoverflow.com/a/29288898
     // see also https://en.wikipedia.org/wiki/Uniform_Resource_Identifier
-    public static final Pattern url = Pattern.compile("(?:https?|ftp)://(?:[\\w_-]+(?:(?:\\.[\\w_-]+)+))(?:/[\\w.,@^=%&:/~+-]+)?(?:\\?[\\w.,@?^=%&:/~+#-]+)?(?:#[\\w.,@?^=%&:/~+#-]+)?");
+    public static final String urlRegex = "(?:https?|ftp)://(?:[\\w_-]+(?:(?:\\.[\\w_-]+)+))(?:/[\\w.,@^=%&:/~+-]+)?(?:\\?[\\w.,@?^=%&:/~+#-]+)?(?:#[\\w.,@?^=%&:/~+#-]+)?";
+    // the regex string is needed for the Link classes in project so-posthistory-extractor
+    public static final Pattern url = Pattern.compile(urlRegex);
 
     // pattern to extract protocol from URL
     public static final Pattern protocol = Pattern.compile("^(https?|ftp)");
