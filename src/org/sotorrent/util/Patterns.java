9db5a30eb5bc8e42a524165ee649d8e37f58df9c
Sebastian Baltes <research@sbaltes.com>
2018-08-15 16:23:57 +0200

    Start refactoring of URL-related code
    * switching to Java's URL class

--- src/org/sotorrent/util/Patterns.java
+++ src/org/sotorrent/util/Patterns.java
@@ -2,6 +2,7 @@ package org.sotorrent.util;
 
 import com.google.common.base.CharMatcher;
 
+import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,7 +18,8 @@ public class Patterns {
     private static final String queryRegex = "\\?[" + allowedCharacters + "\\?]*";
     private static final String fragmentIdentifierRegex = "#[" + allowedCharacters + "?#!]+(?:" + bracketExpression + ")?";
     public static final String urlRegex; // the regex string is needed for the Link classes in project so-posthistory-extractor
-    public static final Pattern url, protocol, completeDomain, rootDomain, path;
+    public static final Pattern url;
+    public static final Pattern rootDomain;
 
     // pattern to detect (valid or malformed) IPv4
     public static final Pattern ipv4 = Pattern.compile("https?://[.\\d]+");
@@ -26,17 +28,8 @@ public class Patterns {
         urlRegex = encloseInNonCapturingGroup(protocolRegex) + "://" + domainRegex + makeOptional(encloseInNonCapturingGroup(pathRegex)) + makeOptional(encloseInNonCapturingGroup(queryRegex)) + makeOptional(encloseInNonCapturingGroup(fragmentIdentifierRegex));
         url = Pattern.compile(urlRegex, Pattern.CASE_INSENSITIVE);
 
-        // pattern to extract protocol from URL
-        protocol = Pattern.compile("^" + encloseInCapturingGroup(protocolRegex), Pattern.CASE_INSENSITIVE);
-
-        // pattern to extract domain (including subdomains) from URL
-        completeDomain = Pattern.compile("^" + encloseInNonCapturingGroup(protocolRegex) + "://" + encloseInCapturingGroup(domainRegex), Pattern.CASE_INSENSITIVE);
-
         // pattern to extract root domain from domain string
         rootDomain = Pattern.compile(rootDomainRegex, Pattern.CASE_INSENSITIVE);
-
-        // pattern to extract path (including query and fragment identifier) from URL
-        path = Pattern.compile(encloseInNonCapturingGroup(protocolRegex) + "://" + domainRegex + makeOptional(encloseInCapturingGroup(pathRegex)) + makeOptional(encloseInCapturingGroup(queryRegex)) + makeOptional(encloseInCapturingGroup(fragmentIdentifierRegex)), Pattern.CASE_INSENSITIVE);
     }
 
     private static String makeOptional(String regex) {
@@ -49,15 +42,6 @@ public class Patterns {
 
     private static String encloseInCapturingGroup(String regex) {
         return "(" + regex + ")";
-    }
-
-    public static String extractProtocolFromUrl(String url) {
-        // protocol
-        Matcher protocolMatcher = Patterns.protocol.matcher(url);
-        if (!protocolMatcher.find()) {
-            throw new IllegalArgumentException("Extraction of protocol failed for URL: " + url);
-        }
-        return protocolMatcher.group(1);
     }
 
     public static String cleanUrl(String url) {
@@ -89,42 +73,32 @@ public class Patterns {
         return backticksBefore > 0 && backticksAfter > 0 && backticksBefore%2 != 0 && backticksAfter%2 != 0;
     }
 
-    public static String extractCompleteDomainFromUrl(String url) {
-        Matcher completeDomainMatcher = Patterns.completeDomain.matcher(url);
-        if (!completeDomainMatcher.find()) {
-            throw new IllegalArgumentException("Extraction of complete domain failed for URL: " + url);
-        }
-        return completeDomainMatcher.group(1);
-    }
-
-    public static String extractRootDomainFromCompleteDomain(String completeDomain) {
-        Matcher rootDomainMatcher = Patterns.rootDomain.matcher(completeDomain);
+    public static String getRootDomain(URL url) {
+        Matcher rootDomainMatcher = Patterns.rootDomain.matcher(url.getHost());
         if (!rootDomainMatcher.find()) {
             throw new IllegalArgumentException("Extraction of root domain failed for URL: " + url);
         }
         return rootDomainMatcher.group(1);
     }
 
-    public static String extractPathFromUrl(String url) {
-        Matcher pathMatcher = Patterns.path.matcher(url);
-        if (!pathMatcher.find()) {
-            // don't change this, needed to set database column to null (so-posthistory-extractor)
-            return null;
-        }
+    public static String getPath(URL url) {
+        String path = url.getPath();
 
-        String path = pathMatcher.group(1);
         // remove leading slash
         if (path.startsWith("/")) {
             path = path.substring(1, path.length());
         }
+
         // remove trailing slash
         if (path.endsWith("/")) {
             path = path.substring(0, path.length()-1);
         }
+
         // return null if path only contains whitespaces
         if (path.trim().length() == 0) {
             return null;
         }
+
         // return null if path only contains punctuation (extracted from Markdown)
         if (path.equals(".") || path.equals(",") || path.equals(":")) {
             return null;
@@ -133,24 +107,20 @@ public class Patterns {
         return path;
     }
 
-    public static String extractQueryFromUrl(String url) {
-        Matcher pathMatcher = Patterns.path.matcher(url);
-        if (!pathMatcher.find()) {
-            // don't change this, needed to set database column to null (so-posthistory-extractor)
-            return null;
+    public static String getQuery(URL url) {
+        String query = url.getQuery();
+        if (query != null && query.trim().length() == 0) {
+            query = null;
         }
-
-        return pathMatcher.group(2);
+        return query;
     }
 
-    public static String extractFragmentIdentifierFromUrl(String url) {
-        Matcher pathMatcher = Patterns.path.matcher(url);
-        if (!pathMatcher.find()) {
-            // don't change this, needed to set database column to null (so-posthistory-extractor)
-            return null;
+    public static String getFragmentIdentifier(URL url) {
+        String fragmentIdentifier = url.getRef();
+        if (fragmentIdentifier != null && fragmentIdentifier.trim().length() == 0) {
+            fragmentIdentifier = null;
         }
-
-        return pathMatcher.group(3);
+        return fragmentIdentifier;
     }
 
     public static boolean isIpAddress(String url) {
