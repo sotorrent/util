e4364b9e87343b0babf70bfdef704cb214f0e780
Sebastian Baltes <research@sbaltes.com>
2018-10-17 10:39:33 +0200

    Fix test case and re-add accidentally removed resource config

--- tests/URLTest.java
+++ tests/URLTest.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.sotorrent.util.URL.validTopLevelDomains;
 
 class URLTest {
     @Test
-    void loadingTldList(){
+    void testLoadingTldList(){
         assertNotNull(validTopLevelDomains);
+        assertTrue(validTopLevelDomains.size() > 0);
     }
 }
