6056c2f211aa7b967fb5f2ce91eada2e8fdf7dd6
Sebastian Baltes <research@sbaltes.com>
2019-03-08 16:53:25 +0100

    Make split method generic

--- src/org/sotorrent/util/collections/CollectionUtils.java
+++ src/org/sotorrent/util/collections/CollectionUtils.java
@@ -3,13 +3,13 @@ package org.sotorrent.util.collections;
 import java.util.List;
 
 public class CollectionUtils {
-    public static List<Integer>[] split(List<Integer> integers, int partitionCount) {
-        int partitionSize = (int)Math.ceil((double)integers.size()/partitionCount);
+    public static <T> List<T>[] split(List<T> list, int partitionCount) {
+        int partitionSize = (int)Math.ceil((double)list.size()/partitionCount);
         @SuppressWarnings("unchecked")
-        List<Integer>[] subLists = new List[partitionCount];
+        List<T>[] subLists = new List[partitionCount];
         int subListIndex = 0;
-        for (int i=0; i<integers.size(); i+=partitionSize) {
-            subLists[subListIndex] = integers.subList(i, Math.min(integers.size(), i+partitionSize));
+        for (int i=0; i<list.size(); i+=partitionSize) {
+            subLists[subListIndex] = list.subList(i, Math.min(list.size(), i+partitionSize));
             subListIndex++;
         }
         return subLists;
